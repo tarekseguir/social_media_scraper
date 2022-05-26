cafe593510bd4c7d289550f238a867ba09bcfc75
tarekseguir <seguirt90@gmail.com>
2022-05-26 15:18:56 +0100

    Make some exception handler ❌🚫

--- src/main.py
+++ src/main.py
@@ -19,16 +19,23 @@ class Scraping_option(BaseModel):
 @app.post("/facebook/{page_name}")
 def get_posts(page_name: str, options: Scraping_option):
     """
-    :param page_name: facebook page that we want to scrap they're posts
-    :param options: the options that we want to define for scraping
+    :param page_name: facebook page that we want to scrap they're posts \n
+    :param options: the options that we want to define for scraping for now it only take the number of pages we want to scrape. \n
     """
-    logging.info(f"Start scraping posts from {page_name}")
-    scraper = Fb_scraper(page_name=page_name)
-    data = scraper.get_posts_info(options.pages)
-    logging.info(f"Scraping completed!")
-    index_name = f"{page_name}_facebook_posts"
+    try:
+        logging.info(f"Start scraping posts from {page_name}")
+        scraper = Fb_scraper(page_name=page_name)
+        data = scraper.get_posts_info(options.pages)
+        logging.info(f"Scraping completed!")
+    except:
+        logging.info(f"{page_name} Page not found!")
+        return {"message": f"{page_name} Page not found!"}
+
+    index_name = page_name.replace(' ','_').lower()
+    index_name = f"{index_name}_facebook_posts"
     es = EsManagement()
     es.create_index(index_name)
     es.populate_index(data, index_name)
+    logging.info(f"{page_name} Page not found!")
+    return {"message": "the page scraping has been completed successfully"}
     
-    return None
